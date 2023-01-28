82cbb2121104b4c9724241ecbc0975fa43294d6e
Henrique Vilela <hv_santos@outlook.com>
2023-01-28 15:42:14 -0300

    feat change globalContext and render component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
 import './App.css';
+import FilterName from './components/FilterName';
+import Header from './components/Header';
 import MyProvider from './context/MyProvider';
 import Home from './pages/Home';
 
 function App() {
   const [planets, setPlanets] = useState(null);
+  const [originPlanets, setOriginPlanets] = useState(null);
+  const [filterName, setFilterName] = useState('');
+  const [filterNumber, setFilterNumber] = useState({
+    filters: [{ colunm: '', comparison: '', value: '' }],
+    numberFilter: false,
+  });
+  const [sortBy, setSortBy] = useState({ by: '', sortFilter: false });
   const globalContext = {
-    planets,
-    setPlanets,
+    planets: {
+      planets,
+      setPlanets,
+    },
+    originalPlanets: {
+      originPlanets,
+      setOriginPlanets,
+    },
+    nameFilter: {
+      filterName,
+      setFilterName,
+    },
+    numberFilter: {
+      filterNumber,
+      setFilterNumber,
+    },
+    sort: {
+      sortBy,
+      setSortBy,
+    },
   };
+
+  console.log(globalContext);
 
   return (
     <MyProvider.Provider value={ globalContext }>
+      <Header />
+      <FilterName />
       <Home />
     </MyProvider.Provider>
   );
