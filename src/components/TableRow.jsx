92257af230e22e5cce29bfb4167b5eef84aee00d
Henrique Vilela <hv_santos@outlook.com>
2023-01-28 15:43:51 -0300

    feat condition to render by filterName

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import MyProvider from '../context/MyProvider';
 
 export default function TableRow() {
-  const { planets } = useContext(MyProvider);
+  const { planets: { planets }, nameFilter: { filterName } } = useContext(MyProvider);
   return (
     <tbody>
       { planets.map((planet) => {
@@ -10,13 +10,16 @@ export default function TableRow() {
           planet.diameter, planet.climate, planet.gravity, planet.terrain,
           planet.surface_water, planet.population, planet.films, planet.created,
           planet.edited, planet.url];
-        return (
-          <tr key={ `${planet.diameter}${planet.name}` }>
-            { arrPlanet.map((tabDesc) => (
-              <td key={ `${planet}${tabDesc}` }>{ tabDesc }</td>
-            )) }
-          </tr>
-        );
+        if (planet.name.toLowerCase().includes(filterName.toLowerCase())) {
+          return (
+            <tr key={ `${planet.diameter}${planet.name}` }>
+              { arrPlanet.map((tabDesc) => (
+                <td key={ `${planet}${tabDesc}` }>{ tabDesc }</td>
+              )) }
+            </tr>
+          );
+        }
+        return '';
       }) }
     </tbody>
   );
