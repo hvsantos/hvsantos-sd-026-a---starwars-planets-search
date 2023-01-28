c7768e388c66340554349e1c701c6bc5410f0dde
Henrique Vilela <hv_santos@outlook.com>
2023-01-28 12:03:55 -0300

    feat use Provider and create globalContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import MyProvider from './context/MyProvider';
+import Home from './pages/Home';
 
 function App() {
+  const [planets, setPlanets] = useState(null);
+  const globalContext = {
+    planets,
+    setPlanets,
+  };
+
   return (
-    <span>Hello, App!</span>
+    <MyProvider.Provider value={ globalContext }>
+      <Home />
+    </MyProvider.Provider>
   );
 }
 
