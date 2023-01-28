003cbc10c1873c9fefe15d77c46af193ae166719
Henrique Vilela <hv_santos@outlook.com>
2023-01-28 18:36:06 -0300

    feat filter in component TableRow

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -2,10 +2,34 @@ import React, { useContext } from 'react';
 import MyProvider from '../context/MyProvider';
 
 export default function TableRow() {
-  const { planets: { planets }, nameFilter: { filterName } } = useContext(MyProvider);
+  const {
+    planets: { planets },
+    nameFilter: { filterName },
+    numberFilter: { filterNumber },
+  } = useContext(MyProvider);
+  function checkFilter() {
+    let checkPlanets = [...planets];
+    filterNumber.forEach((filter) => {
+      checkPlanets = checkPlanets.filter((planet) => {
+        let verify = false;
+        if (filter.comparison === 'maior que') {
+          verify = Number(planet[filter.column]) > Number(filter.value);
+        }
+        if (filter.comparison === 'menor que') {
+          verify = Number(planet[filter.column]) < Number(filter.value);
+        }
+        if (filter.comparison === 'igual a') {
+          verify = Number(planet[filter.column]) === Number(filter.value);
+        }
+        return verify;
+      });
+    });
+    return checkPlanets;
+  }
+  const filteredPlanets = filterNumber.length > 0 ? checkFilter() : planets;
   return (
     <tbody>
-      { planets.map((planet) => {
+      { filteredPlanets.map((planet) => {
         const arrPlanet = [planet.name, planet.rotation_period, planet.orbital_period,
           planet.diameter, planet.climate, planet.gravity, planet.terrain,
           planet.surface_water, planet.population, planet.films, planet.created,
