220fc812732c446adb52ef236aca1b174d8a22d0
Henrique Vilela <hv_santos@outlook.com>
2023-01-28 12:04:36 -0300

    feat Table Component

---
+++ src/components/Table.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HeaderTable from './HeaderTable';
+import TableRow from './TableRow';
+
+export default function Table() {
+  return (
+    <table>
+      <HeaderTable />
+      <TableRow />
+    </table>
+  );
+}
