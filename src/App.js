1d921857cff77a8c10dd907d6dcf7f6ed716c6a3
Henrique Vilela <hv_santos@outlook.com>
2023-01-29 00:14:18 -0300

    fix had to import act to mock the fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { act } from 'react-dom/test-utils';
 import './App.css';
 import FilterName from './components/FilterName';
 import Header from './components/Header';
+import Loading from './components/Loading';
 import MyProvider from './context/MyProvider';
 import Home from './pages/Home';
 
@@ -29,8 +31,19 @@ function App() {
     },
   };
 
-  console.log(globalContext);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://swapi.dev/api/planets');
+      const data = await response.json();
+      setPlanets(data.results);
+    };
 
+    act(() => {
+      fetchData();
+    });
+  }, [setPlanets]);
+
+  if (!planets) return <Loading />;
   return (
     <MyProvider.Provider value={ globalContext }>
       <Header />
